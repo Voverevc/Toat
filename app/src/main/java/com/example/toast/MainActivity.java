2b14e92574f142673957a454c1f83c51730d3baf
Dmitriy <pavelask@gmail.com>
2022-12-06 14:25:25 +0300

    laba5

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonClicked(View view) {
+        Toast myToast = Toast.makeText(getApplicationContext(),
+                "Ура выплавающие собщения работают",
+                Toast.LENGTH_LONG);
+        myToast.setGravity(Gravity.TOP,0,0);
+        myToast.show();
+    }
+}
